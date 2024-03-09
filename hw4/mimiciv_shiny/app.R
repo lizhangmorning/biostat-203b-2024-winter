f4ea0268a99f3e15a1eddefd04d89ae3368e8e60
张立 <zhangli@Zs-Laptop.local>
2024-03-08 23:52:59 -0800

    'ADT' part wrong

--- hw4/mimiciv_shiny/app.R
+++ hw4/mimiciv_shiny/app.R
@@ -18,6 +18,8 @@ library(gt)
 library(gtsummary)
 library(tidyverse)
 
+dataset <- readRDS("/Users/zhangli/203b_hw_new/hw4/mimiciv_shiny/mimic_icu_cohort.rds")
+
 # path to the service account token 
 satoken <- "biostat-203b-2024-winter-313290ce47a6.json"
 # BigQuery authentication using service account
@@ -44,12 +46,11 @@ diagnoses_id <- tbl(con_bq, "d_icd_diagnoses")
 
 
 
-
 ui <- fluidPage(
   titlePanel("Explore the ICU cohort data"),
   fluidRow(
     column(4,  
-           # 创建一个下拉框，让用户选择要展示的 care unit
+           
            selectInput("unit_selector", "Variable of interest:", 
                        choices = c("First Care Unit" = "first_careunit", 
                                    "Last Care Unit" = "last_careunit",
@@ -64,7 +65,7 @@ ui <- fluidPage(
                                    "Lab events" = "lab_events",
                                    "Chart events" = "chart_events"
                        ),
-                       selected = "first_careunit") # 设置默认选择
+                       selected = "first_careunit") 
     )
   ),
   column(8,
@@ -75,48 +76,37 @@ ui <- fluidPage(
            ),
            tabPanel("Table",
                     h5('Patient count'),
-                    plotOutput("table_plot")  # 在这里放置 plotOutput
-           ),tabPanel("Patient's ADT and ICU stay information",
-                      sidebarLayout(
-                        sidebarPanel(
-                          helpText("Select a patient"),
-                          selectizeInput("patient_id", "Patient ID", choices = NULL, options = list(maxItems = 1)),
-                          selectInput("type", "which kind of information",
-                                      choices = c("ADT", "ICU stay"),
-                                      selected = "ADT"),
-                        ),
-                        mainPanel(
-                          plotOutput("adt_plot")
-                        )
-                        
-                    )
+                    plotOutput("table_plot")  
            )
+           # tabPanel("Patient's ADT and ICU stay information",
+           #            sidebarLayout(
+           #              sidebarPanel(
+           #                helpText("Select a patient"),
+           #                selectizeInput("patient_id", "Patient ID", choices = NULL, options = list(maxItems = 1)),
+           #                selectInput("type", "which kind of information",
+           #                            choices = c("ADT", "ICU stay"),
+           #                            selected = "ADT"),
+           #              ),
+           #              mainPanel(
+           #                plotOutput("adt_plot")
+           #              )
+           #              
+           #          )
+           # )
          )
   )
 )
 
 
-
-
-
-
-
-
-
-
-
-
-
 server <- function(input, output) {
+  # observe({
+  #   updateSelectizeInput(session, "patient_id", choices = unique(dataset$subject_id))
+  # })
+
   
-  # 使用 reactive 读取数据框
-  dataset <- reactive({
-    readRDS("./mimiciv_shiny/mimic_icu_cohort.rds")
-  })
-  
-  # 创建 summary_data reactive 对象
+
   summary_data <- reactive({
-    data <- dataset()
+    data <- dataset
     if (!is.null(data)) {
       if (input$unit_selector %in% c("lab_events", "chart_events")) {
 
@@ -130,7 +120,7 @@ server <- function(input, output) {
             "Respiratory Rate", "Temperature Fahrenheit")
         }
         
-        # 使用 summarise_at() 函数根据选定的变量进行汇总统计
+
         data %>%
           summarise_at(vars(all_of(selected_variables)), list(
             mean = ~mean(., na.rm = TRUE),
@@ -158,21 +148,19 @@ server <- function(input, output) {
   }
     })
   
-  # 输出 summary 数据
+
   output$summary <- renderPrint({
     summary_data() %>%
       knitr::kable()
   })
   
   
-  
-  # 响应用户选择的 care unit，并生成柱状图
-  output$table_plot <- renderPlot({  # 修改输出的 ID 为 table_plot
-    # 获取数据框
-    data <- dataset()
+  output$table_plot <- renderPlot({  
+
+    data <- dataset
     
     if (input$unit_selector == "lab_events") {
-      # 选择绘制箱线图
+
       data %>%
         select(
           Bicarbonate, Chloride, Creatinine,
@@ -197,7 +185,7 @@ server <- function(input, output) {
         coord_cartesian(xlim = c(0, 250))
     } else 
       if (input$unit_selector == "chart_events"){
-        # 选择绘制箱线图
+
         data %>%
           select(
             "Heart Rate", "Non Invasive Blood Pressure systolic",
@@ -222,7 +210,7 @@ server <- function(input, output) {
           theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
           coord_cartesian(xlim = c(0, 250))
       } else {
-        # 选择绘制柱状图
+
         data %>%
           ggplot(aes(x = .data[[input$unit_selector]])) +
           geom_bar(stat = "count", fill = "skyblue") +
@@ -234,165 +222,168 @@ server <- function(input, output) {
           coord_flip()
       }
   })
-  
- adt_reactive <- reactive({
-    sub_id <- as.integer(input$patient_id)
-    sid_adt |>
-      filter(subject_id == sub_id & eventtype != "discharge") |>
-      collect()
-  })
-  
-  labevents_reactive <- reactive({
-    sub_id <- as.integer(input$patient_id)
-    labevents_info |>
-      filter(subject_id == sub_id) |>
-      select(charttime) |>
-      collect()
-  })
-  
-  procedures_reactive <- reactive({
-    sub_id <- as.integer(input$patient_id)
-    procedures_info |>
-      filter(subject_id == sub_id) |>
-      select(chartdate, icd_code, icd_version) |>
-      left_join(procedures_id, by = c("icd_code" = "icd_code", "icd_version" = "icd_version")) |>
-      select(chartdate, long_title) |>
-      collect()
-  })
-  
-  diagnoses_reactive <- reactive({
-    sub_id <- as.integer(input$patient_id)
-    diagnoses_info |>
-      filter(subject_id == sub_id) |>
-      select(seq_num, icd_code, icd_version) |>
-      left_join(diagnoses_id, by = c("icd_code" = "icd_code", "icd_version" = "icd_version")) |>
-      select(long_title) |>
-      collect() |>
-      slice(1:3)
-  })
-  
-adt_title_reactive <- reactive({
-    sub_id <- as.integer(input$patient_id)
-    plot_title <- paste("Patient", sub_id, "ICU stays Vitals")
-  })
-
-  chartevents_reactive <- reactive({
-    sub_id <- as.integer(input$patient_id)
-    chartevents_info |>
-      filter(subject_id == sub_id & itemid %in% c(220045, 220179, 220180, 220210, 223761)) |>
-      select(stay_id, charttime, itemid, value) |>
-      collect() |>
-      mutate(
-        itemid = case_when(
-          itemid == 220045 ~ "HR",
-          itemid == 220180 ~ "NBPd",
-          itemid == 220179 ~ "NBPs",
-          itemid == 220210 ~ "RR",
-          itemid == 223761 ~ "Temperature F")) |>
-      mutate(charttime = as.POSIXct(charttime), value = as.numeric(value)) 
-  })
-  
-  plot1_title_reactive <- reactive({
-    sub_id <- as.integer(input$patient_id)
-    patients_info <- mimic_icu_cohort[mimic_icu_cohort$subject_id == sub_id, ]
-    gender <- patients_info$gender[1]
-    age <- patients_info$anchor_age[1]
-    race <- patients_info$race[1]
-    plot_title <- paste("Patient", sub_id, ",", gder, ",", age, "years old", ",", tolower(race))
-  })
-  
-  
-  plot2_title_reactive <- reactive({
-    sub_id <- as.integer(input$patient_id)
-    plot_title <- paste("Patient", sub_id, "ICU stays Vitals")
-  })
-  
-  
-  
-  
-  # 创建 ADT plot
-  output$adt_plot <- renderPlot({
- 
-    req(input$patient_id, input$type)
-    if (is.null(input$patient_id) || input$patient_id == "") {
-      return(NULL)
-    }
-    if (input$type == "ADT") {
-      adt <- adt_reactive()
-      labevents <- labevents_reactive()
-      procedures <- procedures_reactive()
-      diagnoses <- diagnoses_reactive()
-      plot_title <- plot1_title_reactive()
-      
-    
-     
-      
-      plot_subtitle <- paste(diagnoses$long_title[1], "\n", diagnoses$long_title[2], "\n", diagnoses$long_title[3])
-      
-    ggplot() +
-      # admission, discharge, and transfer (ADT)
-      geom_segment(
-        data = adt,
-        mapping = aes(
-          x = intime,
-          xend = outtime,
-          y = "ADT",
-          yend = "ADT",
-          color = careunit,
-          linewidth = str_detect(careunit, "(ICU|CCU)")
-        ),
-      )+
-      # labs
-      geom_point(
-        data = labevents |> distinct(charttime, keep_all = TRUE),
-        mapping = aes(x = charttime, y = "Lab"),
-        shape = "+",
-        size = 5
-      ) +
-      # procedures
-      geom_jitter(
-        # only keep the 1st procedure on the same day
-        data = procedures,
-        mapping = aes(
-          x = chartdate + hours(12),
-          y = "Procedure",
-          shape = str_sub(long_title, 1, 25)
-        ),
-        size = 3,
-        height = 0
-      ) +
-      labs(
-        title = plot_title,
-        subtitle = plot_subtitle,
-        x = "Calendar Time",
-        y = "",
-        color = "Care Unit",
-        shape = "Procedure"
-      ) +
-      guides(linewidth = "none") +
-      scale_y_discrete(limits = rev) +
-      theme_light() +
-      theme(legend.position = "bottom", legend.box = "vertical")
-    }
-    else{
-      plot_title <- plot2_title_reactive()
-      chartevents <- chartevents_reactive()
-      ggplot(chartevents, aes(x = charttime, y = value, color = itemid)) +
-        geom_point() +
-        geom_line() +
-        facet_grid(itemid ~ stay_id, scales = "free") +
-        labs(
-          title = plot_title,
-          x = "",
-          y = ""
-        ) +
-        theme_light() +
-        theme(legend.position = "none")
-    }
-
-})
 }
+  
 
+  
+  
+#  adt_reactive <- reactive({
+#     sub_id <- as.integer(input$patient_id)
+#     sid_adt |>
+#       filter(subject_id == sub_id & eventtype != "discharge") |>
+#       collect()
+#   })
+#   
+#   labevents_reactive <- reactive({
+#     sub_id <- as.integer(input$patient_id)
+#     labevents_info |>
+#       filter(subject_id == sub_id) |>
+#       select(charttime) |>
+#       collect()
+#   })
+#   
+#   procedures_reactive <- reactive({
+#     sub_id <- as.integer(input$patient_id)
+#     procedures_info |>
+#       filter(subject_id == sub_id) |>
+#       select(chartdate, icd_code, icd_version) |>
+#       left_join(procedures_id, by = c("icd_code" = "icd_code", "icd_version" = "icd_version")) |>
+#       select(chartdate, long_title) |>
+#       collect()
+#   })
+#   
+#   diagnoses_reactive <- reactive({
+#     sub_id <- as.integer(input$patient_id)
+#     diagnoses_info |>
+#       filter(subject_id == sub_id) |>
+#       select(seq_num, icd_code, icd_version) |>
+#       left_join(diagnoses_id, by = c("icd_code" = "icd_code", "icd_version" = "icd_version")) |>
+#       select(long_title) |>
+#       collect() |>
+#       slice(1:3)
+#   })
+#   
+# adt_title_reactive <- reactive({
+#     sub_id <- as.integer(input$patient_id)
+#     plot_title <- paste("Patient", sub_id, "ICU stays Vitals")
+#   })
+# 
+#   chartevents_reactive <- reactive({
+#     sub_id <- as.integer(input$patient_id)
+#     chartevents_info |>
+#       filter(subject_id == sub_id & itemid %in% c(220045, 220179, 220180, 220210, 223761)) |>
+#       select(stay_id, charttime, itemid, value) |>
+#       collect() |>
+#       mutate(
+#         itemid = case_when(
+#           itemid == 220045 ~ "HR",
+#           itemid == 220180 ~ "NBPd",
+#           itemid == 220179 ~ "NBPs",
+#           itemid == 220210 ~ "RR",
+#           itemid == 223761 ~ "Temperature F")) |>
+#       mutate(charttime = as.POSIXct(charttime), value = as.numeric(value)) 
+#   })
+#   
+#   plot1_title_reactive <- reactive({
+#     sub_id <- as.integer(input$patient_id)
+#     patients_info <- mimic_icu_cohort[mimic_icu_cohort$subject_id == sub_id, ]
+#     gender <- patients_info$gender[1]
+#     age <- patients_info$anchor_age[1]
+#     race <- patients_info$race[1]
+#     plot_title <- paste("Patient", sub_id, ",", gder, ",", age, "years old", ",", tolower(race))
+#   })
+#   
+#   
+#   plot2_title_reactive <- reactive({
+#     sub_id <- as.integer(input$patient_id)
+#     plot_title <- paste("Patient", sub_id, "ICU stays Vitals")
+#   })
+#   
+#   
+#   
+#   
+#   # 创建 ADT plot
+#   output$adt_plot <- renderPlot({
+#  
+#     req(input$patient_id, input$type)
+#     if (is.null(input$patient_id) || input$patient_id == "") {
+#       return(NULL)
+#     }
+#     if (input$type == "ADT") {
+#       adt <- adt_reactive()
+#       labevents <- labevents_reactive()
+#       procedures <- procedures_reactive()
+#       diagnoses <- diagnoses_reactive()
+#       plot_title <- plot1_title_reactive()
+#       
+#     
+#      
+#       
+#       plot_subtitle <- paste(diagnoses$long_title[1], "\n", diagnoses$long_title[2], "\n", diagnoses$long_title[3])
+#       
+#     ggplot() +
+#       # admission, discharge, and transfer (ADT)
+#       geom_segment(
+#         data = adt,
+#         mapping = aes(
+#           x = intime,
+#           xend = outtime,
+#           y = "ADT",
+#           yend = "ADT",
+#           color = careunit,
+#           linewidth = str_detect(careunit, "(ICU|CCU)")
+#         ),
+#       )+
+#       # labs
+#       geom_point(
+#         data = labevents |> distinct(charttime, keep_all = TRUE),
+#         mapping = aes(x = charttime, y = "Lab"),
+#         shape = "+",
+#         size = 5
+#       ) +
+#       # procedures
+#       geom_jitter(
+#         # only keep the 1st procedure on the same day
+#         data = procedures,
+#         mapping = aes(
+#           x = chartdate + hours(12),
+#           y = "Procedure",
+#           shape = str_sub(long_title, 1, 25)
+#         ),
+#         size = 3,
+#         height = 0
+#       ) +
+#       labs(
+#         title = plot_title,
+#         subtitle = plot_subtitle,
+#         x = "Calendar Time",
+#         y = "",
+#         color = "Care Unit",
+#         shape = "Procedure"
+#       ) +
+#       guides(linewidth = "none") +
+#       scale_y_discrete(limits = rev) +
+#       theme_light() +
+#       theme(legend.position = "bottom", legend.box = "vertical")
+#     }
+#     else{
+#       plot_title <- plot2_title_reactive()
+#       chartevents <- chartevents_reactive()
+#       ggplot(chartevents, aes(x = charttime, y = value, color = itemid)) +
+#         geom_point() +
+#         geom_line() +
+#         facet_grid(itemid ~ stay_id, scales = "free") +
+#         labs(
+#           title = plot_title,
+#           x = "",
+#           y = ""
+#         ) +
+#         theme_light() +
+#         theme(legend.position = "none")
+#     }
+# 
+# })
+# }
 
-shinyApp(ui, server)
+shinyApp(ui = ui, server = server)
 
